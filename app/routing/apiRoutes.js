ed43edc232af882684d965863a0a92976729e657
unknown <dreilingmarshall@gmail.com>
2019-02-21 14:32:32 -0500

    Initial work on comparison logic. Not working due to 404 error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,5 +18,36 @@ module.exports = function(app) {
     // API POST Request
     app.post("/api/friends", function(req, res) {
         friendsData.push(req.body);
+
+        // Setting variables for comparison
+        let newCompare = req.body.responses;
+        let currentCompare = 0;
+        let allCompare = [];
+
+        // For loop to compare new user against each existing user.
+        for (i = 0; i < friendsData.length - 1; i++) {
+            
+            // Reseting current compare value for each user.
+            currentCompare = 0;
+
+            // For loop comparing survey responses between new user and currently selected user(determined by position in for loop)
+            for(j = 0; j < 10; j++) {
+                currentCompare += (Math.abs(parseInt(friendsData[i].responses[j]) - parseInt(newCompare.response[j])));
+            };
+
+            // Pushing current comparison value into array of all users.
+            allCompare.push(currentCompare);
+            
+        };
+
+        // Choose minimum value from the all compare array.
+        let bestMatch = Math.min(...allCompare);
+        let matchPosition = allCompare.indexOf(bestMatch);
+        let match = friendsData[matchPosition];
+
+        // Push match into res callback to send it back to survey to be read by modal.
+        res.json(match);
+
     });
+
 };
