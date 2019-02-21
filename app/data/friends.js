84bc4e57e0b374b1c7e7c0546fb8094f0a370d12
unknown <dreilingmarshall@gmail.com>
2019-02-21 11:54:19 -0500

    Trials of validation. Non-working pushing because .post is not a known function

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,6 +18,38 @@ var friendsData = [
             1,
             1
         ]
+    },
+    {
+        name: "Jane Doe",
+        image: "image.jpg",
+        responses: [
+            1,
+            2,
+            2,
+            1,
+            2,
+            1,
+            2,
+            2,
+            1,
+            2
+        ]
+    },
+    {
+        name: "John Anno",
+        image: "image.jpg",
+        responses: [
+            3,
+            1,
+            4,
+            1,
+            5,
+            1,
+            4,
+            1,
+            3,
+            4
+        ]
     }
 ];
 
